f4ce3354249a6da763341e34c3b02d126f0c666c
FredrikPedersen <fredrikhp@gmail.com>
2019-01-28 12:25:24 +0100

    Ferdig med URL-leseren. Returnerer nå tid og date formatert på riktig måte.

---
+++ Oblig1/src/Exceptions/RegionNotFoundException.java
@@ -0,0 +1,9 @@
+package Exceptions;
+
+import javax.swing.plaf.synth.Region;
+
+public class RegionNotFoundException extends Exception {
+    public RegionNotFoundException(String msg) {
+        super(msg);
+    }
+}
--- Oblig1/src/URLReading/URLReader.java
+++ Oblig1/src/URLReading/URLReader.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
