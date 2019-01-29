5a776b89133aa17263d9d3d69f23273404d31238
nikkip98 <xolmcs@hotmail.com>
2019-01-29 15:58:27 +0100

    Revert Main

--- AllKode/src/Main.java
+++ AllKode/src/Main.java
@@ -3,21 +3,6 @@ import URLReading.URLReader;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        int port = 5555;
-        System.out.println("Starting server! Ready for connection");
-
-        try(
-                ServerSocket serverSocket = new ServerSocket(port);
-        )
-        {
-            while(true) {
-                ConnectSocketThread connectSocketThread = new ConnectSocketThread(serverSocket.accept());
-                //OR   new ConnectSocketThread(serverSocket.accept()).start();
-                connectSocketThread.start();
-            }
-        } catch (IOException e) {
-
-            System.out.println(e.getMessage());
-        }
-}
+        System.out.println(URLReader.reader());
+    }
 }
