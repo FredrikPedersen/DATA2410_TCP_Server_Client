f12870446a203948e3fd2ea8468ec74b7fa7daeb
nikkip98 <xolmcs@hotmail.com>
2019-01-29 15:48:45 +0100

    Laget Multi client connection

--- AllKode/src/Main.java
+++ AllKode/src/Main.java
@@ -3,6 +3,21 @@ import URLReading.URLReader;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        System.out.println(URLReader.reader());
+        int port = 5555;
+        System.out.println("Starting server! Ready for connection");
+
+        try(
+                ServerSocket serverSocket = new ServerSocket(port);
+        )
+        {
+            while(true) {
+                ConnectSocketThread connectSocketThread = new ConnectSocketThread(serverSocket.accept());
+                //OR   new ConnectSocketThread(serverSocket.accept()).start();
+                connectSocketThread.start();
+            }
+        } catch (IOException e) {
+
+            System.out.println(e.getMessage());
+        }
 }
 }
